ae4819a1cb05b4e24ceb7fe306f988bb9c8d32a0
Guilherme Polo <gp@deginner.com>
2015-10-20 14:40:10 -0500

    pypi does not parse the full README correctly, so restrict it to top section

--- setup.py
+++ setup.py
@@ -10,14 +10,24 @@ classifiers = [
     "Topic :: Security :: Cryptography"
 ]
 
+url = 'https://github.com/g-p-g/bitjws'
+
+# Cut the readme before the second section start.
+top = readme.find('Usage\n-----')
+bottom = readme.find('.. |Build Status|')
+more = ("Visit {} to see the full README, the\n"
+        "issue tracker, and others.\n\n\n")
+readme = readme[:top] + more.format(url) + readme[bottom:]
+
+
 setup(
     name="bitjws",
-    version="0.6.1",
+    version="0.6.2",
     description='JWS using Bitcoin message signing',
     long_description=readme,
     author='Guilherme Polo',
     author_email='gp@deginner.com',
-    url='https://github.com/g-p-g/bitjws',
+    url=url,
     license='MIT',
     classifiers=classifiers,
     include_package_data=True,
